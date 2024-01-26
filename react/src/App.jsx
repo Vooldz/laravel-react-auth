e7b659c4e59e226770fca005d95850fada70427a
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-01-25 23:21:36 -0800

    create the initial design for the card list

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,25 @@
-//components
-import { useEffect } from "react"
-import Header from "./components/header/Header"
+import { useEffect, useState } from "react"
 import axiosClient from "./axiosClient"
+
+//components
+import Header from "./components/header/Header"
+import Home from "./pages/Home"
 function App() {
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
-    axiosClient.get('/customers').then(response => {
-      console.log('Customers: ', response.data)
+    axiosClient.get('/categories').then(response => {
+      console.log('categories: ', response.data)
+    })
+    axiosClient.get('/products').then(response => {
+      console.log('products: ', response.data)
     })
   }, [])
 
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   )
 }
---
+++ react/src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import Sections from '../components/layout/Sections'
+
+const Products = () => {
+    return (
+        <div>
+            <Sections />
+        </div>
+    )
+}
+
+export default Products
