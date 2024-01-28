223c6d31c6b2f34d994e25689e8980195acfe8ad
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-01-28 12:13:57 -0800

    starting the initial layout

---
+++ react/src/components/layout/Sections.jsx
@@ -0,0 +1,12 @@
+import Cards from './Cards'
+
+const Sections = () => {
+    return (
+        <div>
+            <h1 className="section-title">title</h1>
+            <Cards />
+        </div>
+    )
+}
+
+export default Sections
---
+++ react/src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import Sections from '../components/layout/Sections'
+
+const Products = () => {
+    return (
+        <div>
+            <Sections />
+            
+        </div>
+    )
+}
+
+export default Products
