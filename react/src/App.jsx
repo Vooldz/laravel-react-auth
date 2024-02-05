8a6b946488361b6dbcf03645f6c6d9879f2ad43c
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-02-05 15:22:28 -0800

    .

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,19 +1,17 @@
-// import { useEffect, useState } from "react"
-// import axiosClient from "./axiosClient"
 
 //components
 import Header from "./components/header/Header"
-// import Home from "./pages/Home"
-// import Footer from "./components/footer/Footer"
+import Home from "./pages/Home"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/*  routes here... */}
-      {/* <Home /> */}
+      <Home />
       
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
