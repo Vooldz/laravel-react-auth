ac72b028ec1f54d8d177b18a709917291574ca88
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-02-18 16:57:15 -0800

    change nav links

--- react/src/components/layout/NavLinks.jsx
+++ react/src/components/layout/NavLinks.jsx
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 
 const NavLinks = ({ style }) => (
     <ul className={`flex ${style}`}>
-        {["link1", "link2", "link3", "link4"].map(link => (
-            <li key={link}><Link to={link} className="button icon">{link}</Link></li>
+        {["Home", "shpping", "contact us"].map(link => (
+            <li key={link}><Link to={link === "Home" ? '/' : link} className="button icon">{link}</Link></li>
         ))}
     </ul>
 )
