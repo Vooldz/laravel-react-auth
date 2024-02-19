d68a20407a7d8396b71f8859644289b5379a9afc
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-02-18 18:01:23 -0800

    chnge the root colors

--- react/src/components/layout/Search.jsx
+++ react/src/components/layout/Search.jsx
@@ -1,28 +1,20 @@
-/* eslint-disable react/prop-types */
-import { useState, useEffect } from 'react'
+/**
+ * working on this component is stoped till adding the search logic in the backend!
+ */
 
-import axiosClient from "../../axiosClient"
-// import axios from "axios"
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
 
 // icons
 import { IoMdSearch } from 'react-icons/io'
 
 const Search = ({ style }) => {
-    const [categories, setCategories] = useState([])
-    const [searchTerm, setSearchTerm] = useState("")
-
-    useEffect(() => {
-        axiosClient.get(`/products?name=${searchTerm}`).then(response => {
-            console.log(response.data.data);
-            // if (response.status === 200) {
-            // }
-        })
-    }, [searchTerm])
+    const [searchTerm, setSearchTerm] = useState("");
 
 
     return (
-        <div className={`flex ${style}`}>
-            <div className="flex full-width transparent">
+        <div className={`box ${style}`}>
+            <div className="box full-width transparent">
                 <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} type="text" placeholder="Search..." />
                 <button className="icon"><IoMdSearch color="white" /></button>
             </div>
